c5b9a9168910690e28461ef29a4f1ab04f40da63
hauks96 <agir96@gmail.com>
2022-02-17 11:56:39 +0000

    five not four1

---
+++ LabFive/PlayerAvatar.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+
+class PlayerAvatar
+{
+public:
+	PlayerAvatar(Ogre::SceneManager* scene_manager, std::string mesh_file_name);
+	void Update(Ogre::Real delta_time, const Uint8* state);
+	void Move(Ogre::Vector3 translate_vector, float rotation, Ogre::Real delta_time);
+	Ogre::Radian GetRotation(const Ogre::Vector3& vec);
+	void SetIdleAnimationLoop();
+	void SetWalkAnimationLoop();
+	void StopAnimationLoop() const;
+	void StartAnimationLoop() const;
+	Ogre::SceneNode* getEntityNode();
+private:
+	Ogre::SceneManager* _scene_manager;
+	Ogre::Entity* _entity;
+	Ogre::SceneNode* _entity_node;
+	Ogre::AnimationState* _animation_state_base;
+	Ogre::AnimationState* _animation_state_top;
+	float _speed;
+	float _rotation;
+	float _rotation_speed;
+};
+
